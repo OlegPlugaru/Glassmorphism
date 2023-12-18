5aa61fc7bb0224aa6502ec551a8817314029b6eb
OlegPlugaru <olegplugaru.md@gmail.com>
2023-12-18 19:21:14 +0200

    Initial Commit

---
+++ Glassmorphism/GlassmorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismApp.swift
+//  Glassmorphism
+//
+//  Created by Oleg Plugaru on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
